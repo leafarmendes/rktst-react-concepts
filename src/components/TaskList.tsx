b0c8979180963533752045b9fd6a38bd32cc593b
Rafael Mendes <rafaelmendesbusi@gmail.com>
2022-05-11 20:52:46 -0300

    First challenge completed!

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -14,16 +14,37 @@ export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
+
+
+
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle) return;
+
+    const newTask = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+
+    setTasks(prevTask => [...prevTask, newTask]);
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newMap = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    } : task)
+
+    setTasks(newMap)
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    // Remova a task pelo id
+    const removeTask = tasks.filter(task => task.id !== id)
+
+    setTasks(removeTask)
   }
 
   return (
